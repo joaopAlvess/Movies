8fffcf6dc192bb3f43b213a96c26d6657559e372
joaopAlvess <jpfox.alves@gmail.com>
2023-12-16 11:21:10 -0300

    teste acertivo

---
+++ src/test/java/com/movies/fox/FoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.fox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
